b4b093699a7b227053c554cacde0a4dc454afade
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-08 16:57:46 +0530

    ADDED:Frequency method to find the frequency in the paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -8,15 +8,30 @@ namespace HashTable
         {
             Console.WriteLine("Hashtable");
             MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            //Adds details to hash
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
-            Function function = new Function(); 
-            //Calls frequencyOfWord method 
+            Function function = new Function();
+            //Adds details to 
+            //uc1
+            /* hash.Add(0, "To");
+             hash.Add(1, "be");
+             hash.Add(2, "or");
+             hash.Add(3, "not");
+             hash.Add(4, "to");
+             hash.Add(5, "be");
+             //Calls frequencyOfWord method 
+             function.FrequencyOfWord(hash);*/
+            //UC2
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            //Split converts paragraph  into array of sub strings
+            string[] para = paragraph.Split(" ");
+            int key = 0;
+            //Loop iterates on paragraph and adds key and value of each substing
+            //To hash
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            //Calls Frequency of word method to print word count
             function.FrequencyOfWord(hash);
             Console.ReadKey();
         }
