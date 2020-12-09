7c7780aebb2dc47d2457af5f6836bb693cbf2be8
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-09 13:08:59 +0530

    ADDED:remveMethod to remove the data

--- HashTable/Function.cs
+++ HashTable/Function.cs
@@ -24,5 +24,24 @@ namespace HashTable
                 Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
             }
         }
+
+        /// <summary>
+        /// Removes the specified hash.
+        /// </summary>
+        /// <param name="hash">The hash.</param>
+        /// <param name="word">The word.</param>
+        public void Remove(MyMapNode<int, string> hash, string word)
+        {
+            //Loop iterates across the hash to compare key with the word 
+            //if passed word matches with the key then hash.remove removes that value
+            for (int key = 0; key < hash.size; key++)
+            {
+                if (hash.Get(key).Equals(word))
+                {
+                    hash.Remove(key);
+                    Console.WriteLine($"Removed {word} from paragraph");
+                }
+            }
+        }
     }
 }
